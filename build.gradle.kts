76412e93f3e10782d9f24e8c70d6f38ea2a76922
AimanYosofi <=>
2025-05-05 21:41:03 +0300

    initialize the project

---
+++ build.gradle.kts
@@ -0,0 +1,40 @@
+plugins {
+    kotlin("jvm") version "1.9.23"
+}
+
+group = "com.usc.team"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    implementation("io.insert-koin:koin-core:4.0.2")
+    implementation("io.arrow-kt:arrow-core:2.0.1")
+    implementation("io.arrow-kt:arrow-fx-coroutines:2.0.1")
+    implementation("io.insert-koin:koin-core:4.0.2")
+    implementation("com.github.doyaaaaaken:kotlin-csv-jvm:1.9.0")
+    testImplementation("org.jetbrains.kotlin:kotlin-test")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.12.0-M1")
+    testImplementation("io.mockk:mockk:1.13.16")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:5.10.0")
+    testImplementation("com.google.truth:truth:1.4.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.3")
+
+    implementation("io.ktor:ktor-client-core:2.3.13")
+    implementation("io.ktor:ktor-client-cio:2.3.13")
+
+    implementation("ch.qos.logback:logback-classic:1.5.6")
+
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.0")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(17)
+}
