0ca87e111d372407090220954fb5c4a5e15c168b
Aseel AbuKmail <aseelabukmail@gmail.com>
2025-05-08 01:10:20 +0300

    fix issues in build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,9 +36,10 @@ dependencies {
     implementation("io.ktor:ktor-client-cio:$ktor_version")
     implementation("io.ktor:ktor-client-content-negotiation:$ktor_version")
     implementation("io.ktor:ktor-serialization-kotlinx-json:$ktor_version")
+    implementation(kotlin("stdlib-jdk8"))
 
 
-    }
+}
 
 tasks.test {
     useJUnitPlatform()
